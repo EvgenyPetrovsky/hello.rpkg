0747ab98447ec769ed77edeecab0b1b61cf362ef
EvgenyPetrovsky <fonpetroff@mail.ru>
2018-03-31 22:35:49 +0200

    add new "add" function

--- R/hello.R
+++ R/hello.R
@@ -2,3 +2,14 @@
 hello <- function () {
   print("Hello World!")
 }
+
+#' Add two numbers
+#' @description The function works with numbers and does arithmrtic addition
+#' @param x A number
+#' @param y A number
+#' @return sum of two numbers: \code{x} and \code{y}
+#' @example
+#' add(1, 2)
+add <- function(x, y) {
+  x + y
+}
