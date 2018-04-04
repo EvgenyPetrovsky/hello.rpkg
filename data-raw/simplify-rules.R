3d4152ef04708f6deecd5bec362f20fe541d7d45
EvgenyPetrovsky <fonpetroff@mail.ru>
2018-04-04 22:44:00 +0200

    refine code according to pull request comments

--- data-raw/simplify-rules.R
+++ data-raw/simplify-rules.R
@@ -1,9 +1,8 @@
-# load csv
+# load csv and convert into data-types (Fixed field into Boolean)
 simplify.rules <- read.csv(
   file = "./data-raw/simplify-rules.csv",
   header = T, sep = ";", quote = '"',
   colClasses = c("numeric", "character", "character", "logical")
 )
-devtools::use_data(simplify.rules)
-# convert into data-types (Fixed field into Boolean)
 # store as Rdata
+devtools::use_data(simplify.rules)
